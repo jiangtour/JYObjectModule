28d8a2b9fe49976e044eb638d7c2265eac0b4715
aiXing <862099730@qq.com>
2016-08-23 17:14:18 +0800

    Initial Commit

---
+++ JYObjectModule/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYObjectModule
+//
+//  Created by devedbox on 16/8/23.
+//  Copyright © 2016年 jiangyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYObjectModule/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYObjectModule
+//
+//  Created by devedbox on 16/8/23.
+//  Copyright © 2016年 jiangyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
